31567526814033e3080202fd0b1f26701e240797
unknown <ot78.25@ot78-25.weall.lo>
2013-02-12 17:08:28 +0400

    push

---
+++ other-try.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+if __name__ == '__main__':
+    test_list = ['Asd fg', 
+                 'Dfgghf hgghg',
+                 'Lkkkff kfgh']
+    for it in test_list:
+        #print it
+        print it.split(' ')
+        
+copywrite
