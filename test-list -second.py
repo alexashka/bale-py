352dd808e8c7a457fe6265e46377ab9e3e31224a
Your Name <you@example.com>
2013-03-05 11:52:05 +0400

    push

---
+++ test-list -second.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+def _all_first_item_to_upper_and_remove_spaces(sentence):
+    """ ����������...
+    Args:
+    
+    Returns:
+    """
+    # ������ �� ���������
+    result_string = ''
+    space_was = False
+    for at in sentence:
+        if at != ' ':
+            if space_was:
+                result_string += at.upper()
+                space_was = False
+            else:
+                result_string += at
+        else:
+            space_was = True
+    return result_string
+
+def run():
+    test_list = ['Asd dfgdf', 
+                 'Dfgghf hgghg',
+                 'Lkkkff kfgh']
+    for it in test_list:
+        print _all_first_item_to_upper_and_remove_spaces(it)
+        
+        #print it
+        #print it.split(' ')
+    
+if __name__ == '__main__':
+   run()
+
+        
+    
